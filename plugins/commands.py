bed8ac63729970f96198b7bee91888ed9cbceb8f
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-29 21:21:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -30,8 +30,8 @@ async def start(client, message):
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
-        await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
+        await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup, disable_web_page_preview=True)
+        await asyncio.sleep(0.5) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
         if not await db.get_chat(message.chat.id):
             total=await client.get_chat_members_count(message.chat.id)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
@@ -52,9 +52,9 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker="CAACAgIAAxkBAAECruFiurKqVfsKP1cPVPdNZJmwJPMOyAACVBYAAtB7QUn8uVjZ80ZWKB4E",
-            
+            sticker="CAACAgIAAxkBAAECruFiurKqVfsKP1cPVPdNZJmwJPMOyAACVBYAAtB7QUn8uVjZ80ZWKB4E",            
             reply_markup=reply_markup,
+            disable_web_page_preview=True,
             
         )
         return
@@ -98,10 +98,9 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_sticker(
-            sticker="CAACAgIAAxkBAAECruFiurKqVfsKP1cPVPdNZJmwJPMOyAACVBYAAtB7QUn8uVjZ80ZWKB4E",
-            
+            sticker="CAACAgIAAxkBAAECruFiurKqVfsKP1cPVPdNZJmwJPMOyAACVBYAAtB7QUn8uVjZ80ZWKB4E",            
             reply_markup=reply_markup,
-            
+            disable_web_page_preview=True,
         )
         return
     data = message.command[1]
