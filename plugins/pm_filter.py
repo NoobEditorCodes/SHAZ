7056c887308b26eca4d979b22191a23a4cf3d300
CLÆ͜͡Ｙ [DC-1] <78960870+CLaY9950@users.noreply.github.com>
2022-06-29 14:23:04 +0530

    Try

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -431,7 +431,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🔮 𝐒𝐓𝐀𝐓𝐔𝐒', callback_data='stats')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
+        await query.message.delete()
+        await query.message.reply_text(
             text=script.HELP_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode='html'
@@ -446,7 +447,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                                 
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
+        await query.message.delete()
+        await query.message.reply_text(
             text=script.ABOUT_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
