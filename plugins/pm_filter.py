11a04afd4f91e52a8991f5b7ddb6ddd1bc5b56a6
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-06-29 21:53:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -413,8 +413,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('🍁 𝐀𝐁𝐎𝐔𝐓', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-        await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+        #await query.message.edit_text(
+        await client.send_sticker(
+            sticker="CAACAgIAAxkBAAECruFiurKqVfsKP1cPVPdNZJmwJPMOyAACVBYAAtB7QUn8uVjZ80ZWKB4E",            
+            #text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
